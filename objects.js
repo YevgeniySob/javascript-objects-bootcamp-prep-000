7ade771410f8e6c058bbc968ba9308d6a006875c
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-02 20:40:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(playlst, artistName, song){
   playlst[artistName] = song
   return playlst
 }
+
+function removeFromPlaylist(playlst, artistName){
+  delete playlst.artistName
+  return playlst
+}
